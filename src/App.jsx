5ef5f8befa5377a3b77d8ae865397e42c58b841c
belghoul <belghoul@systemd.se>
2022-11-08 18:34:33 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { FlexContent, Hero, Sales } from "./components";
+import {
+  heroapi,
+  popularsales,
+  toprateslaes,
+  highlight,
+  sneaker,
+} from "./data/data";
+
+const App = () => {
+  return (
+    <>
+      <main className="flex flex-col gap-16 relative">
+        <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
+        <Sales endpoint={toprateslaes} />
+        <FlexContent endpoint={sneaker} />
+      </main>
+    </>
+  );
+};
+
+export default App;
