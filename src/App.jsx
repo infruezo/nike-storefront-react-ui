34336abeaf0c21570142f9b07811d8a6878be995
belghoul <belghoul@systemd.se>
2022-11-09 12:20:38 +0100

    Added first version of navbar + stories + footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
 import React from "react";
-import { FlexContent, Hero, Sales } from "./components";
+import {
+  FlexContent,
+  Footer,
+  Hero,
+  Navbar,
+  Sales,
+  Stories,
+} from "./components";
 import {
   heroapi,
   popularsales,
   toprateslaes,
   highlight,
   sneaker,
+  story,
+  footerAPI,
 } from "./data/data";
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <main className="flex flex-col gap-16 relative">
         <Hero heroapi={heroapi} />
         <Sales endpoint={popularsales} ifExists />
         <FlexContent endpoint={highlight} ifExists />
         <Sales endpoint={toprateslaes} />
         <FlexContent endpoint={sneaker} />
+        <Stories story={story} />
       </main>
+      <Footer footerAPI={footerAPI} />
     </>
   );
 };
